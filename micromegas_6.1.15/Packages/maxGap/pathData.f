615bbc50e854e70704a3ed0b0b161c27cf591bce
HCR264 <haroldcruzramirez@gmail.com>
2024-09-18 13:47:36 -0600

    Edición rápida

---
+++ micromegas_6.1.15/Packages/maxGap/pathData.f
@@ -0,0 +1,7 @@
+
+         BLOCK DATA 
+         character*1000 PATH, FILENAME
+         COMMON/maxGapPath/PATH,FILENAME
+         DATA PATH/'/home/harold/Documentos/GitHub/Amalgama/micromegas_6.1.15/Packages/maxGap'/
+         END
+
